eca1fbc574f9f44c56ab7c8620524ba0b03a277f
Gabriel <gschmitz.geomais@gmail.com>
2018-10-25 11:37:29 -0300

    Fontend

--- application/app_backend/src/config/server.js
+++ application/app_backend/src/config/server.js
@@ -2,10 +2,14 @@ const PORT = 3003
 const bodyParse = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 server.use(bodyParse.urlencoded({extended: true}))
 server.use(bodyParse.json())
+server.use(allowCors)
 
 server.listen(PORT, function() {
     console.log('BACKEND está rodando na porta ' + PORT + '.' )
 })
+
+module.exports = server
